2af2ecf656d752be098d8923e2a9fbb63b8c5c48
Jiyoung Ko <jiyoungko@192.168.68.113>
2022-11-29 09:24:12 +0200

    form inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,77 +1,87 @@
-import React, {useState} from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
-import {countries} from "./countries";
-import "./Form.css"
-import dayjs, { Dayjs } from 'dayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { useForm, Controller } from "react-hook-form";
+import Stack from "@mui/material/Stack";
+import { countries } from "./countries";
+import "./Form.css";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { MenuItem, Select, TextField } from "@mui/material";
 
-type Inputs = {
-  example: string,
-  exampleRequired: string,
+type FormValues = {
+  country: string;
+  startDate: string;
+  endDate: string;
 };
 
 export default function Form() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormValues>();
 
-  const [startDate, setStartDate] = useState<Dayjs | null>(
-    dayjs(),
-  );
-  const [endDate, setEndDate] = useState<Dayjs | null>(
-    dayjs(),
-  );
-
-  const handleStartDateChange = (newValue: Dayjs | null) => {
-    setStartDate(newValue)
-  };
-  const handleEndDateChange = (newValue: Dayjs | null) => {
-    setEndDate(newValue)
-  };
-
-  console.log(watch("example")) // watch input value by passing the name of it
-
+  const generateSelectOptions = () => {
+    return countries.map((option) => {
+      return (
+        <MenuItem key={option} value={option}>
+          {option}
+        </MenuItem>
+      );
+    });}
+  
   return (
+    <div className="form__background">
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <Stack spacing={3} sx={{ width: 300 }}>
+          <Controller
+            name="country"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value } }) => (
+              <Select onChange={onChange} value={value}>
+              {generateSelectOptions()}
+            </Select>
+            )}
 
-    <form onSubmit={handleSubmit(onSubmit)}>
-     <Stack spacing={3} sx={{ width: 300 }}>
-     <Autocomplete
-      disablePortal
-      id="country-form"
-      options={countries}
-      sx={{ width: 300, border: '1px solid white', color : "white" }}
-      renderInput={(params) => <TextField sx={{ input: { color: 'red' } }}  {...params} label="Country" />}
-    />
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DesktopDatePicker
-            label="Start Date"
-            inputFormat="MM/DD/YYYY"
-            value={startDate}
-            onChange={handleStartDateChange}
-            renderInput={(params) => <TextField {...params} />}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Controller
+              name="startDate"
+              defaultValue={`${dayjs()}`}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <DesktopDatePicker
+                  label="Start Date"
+                  inputFormat="MM/DD/YYYY"
+                  value={value}
+                  onChange={onChange}
+                  InputProps={{ className: "textfield__label" }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              )}
             />
-        <DesktopDatePicker
-        label="End Date"
-        inputFormat="MM/DD/YYYY"
-        value={endDate}
-        onChange={handleEndDateChange}
-        renderInput={(params) => <TextField {...params} />}
-        />
-    </LocalizationProvider>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-      
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      </Stack>
-      <input type="submit" />
-    </form>
+            <Controller
+              name="endDate"
+              defaultValue={`${dayjs()}`}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <DesktopDatePicker
+                  label="End Date"
+                  inputFormat="MM/DD/YYYY"
+                  value={value}
+                  onChange={onChange}
+                  InputProps={{ className: "textfield__label" }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              )}
+            />
+          </LocalizationProvider>
+          {/* errors will return when field validation fails  */}
+          {errors.startDate && <span>This field is required</span>}
+        </Stack>
+        <input type="submit" value="Add Trip" className="submitButton" />
+      </form>
+    </div>
   );
 }
-
